a388acb105de6b777f30d488dbfa6c2594002e1e
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-09-20 15:19:21 +0800

    Initial Commit

---
+++ browser/browserApp.swift
@@ -0,0 +1,17 @@
+//
+//  browserApp.swift
+//  browser
+//
+//  Created by Adon Omeri on 20/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct browserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
