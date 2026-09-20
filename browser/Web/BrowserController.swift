fd26a33aaa0470cb170e37aaef5072c992b95c4d
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-09-20 21:49:30 +0800

    rename

--- browser/Web/BrowserController.swift
+++ browser/Web/BrowserController.swift
@@ -4,7 +4,7 @@ import WebKit
 @MainActor
 @Observable
 final class BrowserController: NSObject {
-	private static let startURL = URL(string: "https://www.google.com/search?q=colourful+images")!
+	private static let startURL = URL(string: "https://www.google.com/search?newwindow=1&sca_esv=a3e7c45b0e9db7ae&sxsrf=APpeQntVo1xGA_gL9UdIPDxnCIu0EkDvBQ:1789912085476&udm=2&fbs=ABfTbFVyMZGZf1hfvX9uKjN_-G8c4u0nXx4bEIpwm1lnNH832SMIiTl3t-JZ4hGJOxPbHYRQDtz5om-6srRFNP4UK_6edrYqFyYjVoLL0-Wmdmyc0u13h2Xl3P-7Fmh0o6KIonK33XPiaENqVug3Wz4snvosAa7jzH7Rt_v0NrmMfzHNS_lQU-uhKoPgJ8pgOjL1g7N337ePchsb39T_poIr_wqV3CYjsA&q=colourful+images&sa=X&ved=2ahUKEwiHq6KQpv2WAxXtIkQIHZ09Pe8QtKgLegQIFhAB&biw=1220&bih=943&dpr=2")!
 
 	@ObservationIgnored
 	let webView = WKWebView()
