78fa1a17a33d538f59fd871a69f4c247142954fa
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-09-24 20:34:05 +0800

    better

--- browser/Web/BrowserController.swift
+++ browser/Web/BrowserController.swift
@@ -33,6 +33,7 @@ final class BrowserController: NSObject {
 		let previous;
 		const check = () => {
 			scheduled = false;
+			if (window.scrollY < 0) return;
 			const counts = new Map();
 			const samples = 20;
 			for (let index = 0; index < samples; index++) {
@@ -258,6 +259,7 @@ final class BrowserController: NSObject {
 						return
 					}
 					self.hasDeclaredThemeColor = true
+					webView.underPageBackgroundColor = color.withAlphaComponent(1)
 					self.updateThemeColor(color)
 				}
 			},
@@ -400,7 +402,6 @@ final class BrowserController: NSObject {
 				return
 			}
 		#endif
-
 		var red: CGFloat = 0
 		var green: CGFloat = 0
 		var blue: CGFloat = 0
@@ -439,13 +440,6 @@ final class BrowserController: NSObject {
 		#if os(macOS)
 			previewSnapshot = image
 		#endif
-
-		guard !hasDeclaredThemeColor,
-		      let cgImage = Self.cgImage(from: image),
-		      let color = Self.dominantPageColor(in: cgImage)
-		else { return }
-
-		updateThemeColor(color)
 	}
 
 	#if os(macOS)
@@ -464,82 +458,6 @@ final class BrowserController: NSObject {
 			}
 		}
 	#endif
-
-	private static func cgImage(from image: SnapshotImage) -> CGImage? {
-		#if os(iOS)
-			image.cgImage
-		#elseif os(macOS)
-			image.cgImage(forProposedRect: nil, context: nil, hints: nil)
-		#endif
-	}
-
-	private static func dominantPageColor(in image: CGImage) -> PlatformColor? {
-		let width = 40
-		let height = 40
-		var pixels = [UInt8](repeating: 0, count: width * height * 4)
-		let didDraw = pixels.withUnsafeMutableBytes { buffer -> Bool in
-			guard let context = CGContext(
-				data: buffer.baseAddress,
-				width: width,
-				height: height,
-				bitsPerComponent: 8,
-				bytesPerRow: width * 4,
-				space: CGColorSpaceCreateDeviceRGB(),
-				bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue | CGBitmapInfo.byteOrder32Big.rawValue
-			) else { return false }
-			context.draw(image, in: CGRect(x: 0, y: 0, width: width, height: height))
-			return true
-		}
-		guard didDraw else { return nil }
-
-		var chromatic: [Int: (count: Int, red: Int, green: Int, blue: Int)] = [:]
-		var black = 0
-		var white = 0
-		for index in stride(from: 0, to: pixels.count, by: 4) {
-			let red = Int(pixels[index])
-			let green = Int(pixels[index + 1])
-			let blue = Int(pixels[index + 2])
-			let maximum = max(red, green, blue)
-			let minimum = min(red, green, blue)
-			let saturation = maximum == 0 ? 0 : Double(maximum - minimum) / Double(maximum)
-			if saturation < 0.18 {
-				if 0.2126 * Double(red) + 0.7152 * Double(green) + 0.0722 * Double(blue) < 128 {
-					black += 1
-				} else {
-					white += 1
-				}
-				continue
-			}
-
-			let delta = maximum - minimum
-			var hue: Double = if maximum == red {
-				Double(green - blue) / Double(delta)
-			} else if maximum == green {
-				2 + Double(blue - red) / Double(delta)
-			} else {
-				4 + Double(red - green) / Double(delta)
-			}
-			hue = (hue * 60).truncatingRemainder(dividingBy: 360)
-			if hue < 0 {
-				hue += 360
-			}
-			let hueBucket = Int(hue / 15)
-			let saturationBucket = min(2, Int(saturation * 3))
-			let key = hueBucket * 3 + saturationBucket
-			let bucket = chromatic[key, default: (0, 0, 0, 0)]
-			chromatic[key] = (bucket.count + 1, bucket.red + red, bucket.green + green, bucket.blue + blue)
-		}
-
-		let dominant = chromatic.values.max { $0.count < $1.count }
-		let neutralCount = max(black, white)
-		if let dominant, dominant.count >= 320, dominant.count >= neutralCount / 3 {
-			let red = CGFloat(dominant.red) / CGFloat(dominant.count) / 255
-			let green = CGFloat(dominant.green) / CGFloat(dominant.count) / 255
-			let blue = CGFloat(dominant.blue) / CGFloat(dominant.count) / 255
-			return PlatformColor(red: red, green: green, blue: blue, alpha: 1)
-		}
-		return black >= white ? PlatformColor.black : PlatformColor.white
-	}
 }
 
 extension BrowserController: WKNavigationDelegate {
@@ -615,12 +533,13 @@ extension BrowserController: WKNavigationDelegate {
 		historyManager.cancelVisit()
 	}
 
-	func webView(_: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+	func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
 		currentNavigation = navigation
 		awaitsNavigationCommit = true
 		navigationGeneration += 1
 		hasDeclaredThemeColor = false
 		hasTopEdgeContent = false
+		webView.underPageBackgroundColor = nil
 	}
 
 	func webView(_: WKWebView, didCommit navigation: WKNavigation!) {
@@ -629,9 +548,14 @@ extension BrowserController: WKNavigationDelegate {
 		updateHistory()
 	}
 
-	func webView(_: WKWebView, didFinish navigation: WKNavigation!) {
+	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
 		guard navigation === currentNavigation else { return }
 		updateHistory()
+		if !hasDeclaredThemeColor,
+		   let pageBackgroundColor = webView.underPageBackgroundColor
+		{
+			updateThemeColor(pageBackgroundColor)
+		}
 		restoreScrollPositionIfNeeded(in: webView)
 		let generation = navigationGeneration
 		if let url {
